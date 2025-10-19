a499138d6ce6922587c8daa9f031b5a28844c45e
Vladimir Kipriyanov <liar2707@gmail.com>
2025-10-19 22:43:28 +0500

    добавил первый шаг 2.1

--- public/index.php
+++ public/index.php
@@ -1,3 +1,31 @@
 <?php
 
-print_r($_SERVER);
+// Подключение автозагрузки через composer
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
+use DI\Container;
+
+//session_start();
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+//$app->add(new MethodOverrideMiddleware());
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) use ($app) {
+    $params = [];
+    return $this->get('renderer')->render($response, "/home.phtml", $params);
+})->setName('home');
+
+$app->run();
